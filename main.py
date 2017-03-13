fe8f9ceeee1fe0c8ffdaa885717faf3174797ef7
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2017-03-13 21:50:45 +0100

    eye recognition, minor refactor, fixed url parsed as command

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import cv2
 import numpy as np
 import re
+import math
 
 from PIL import Image
 import requests
@@ -11,6 +12,7 @@ from io import BytesIO
 
 client = discord.Client()
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 img_face_replace = Image.open('face.png')
 
 @client.event
@@ -28,9 +30,9 @@ async def on_message(message):
         if len(message.attachments) > 0:
             urls = [message.attachments[0]['url']]
 
-        if 'load' in message.content:
+        if 'load' in message.content.strip(urls[0]):
             await face_load_handler(message, urls[0])
-        elif 'print' in message.content:
+        elif 'print' in message.content.strip(urls[0]):
             await client.send_file(message.channel,
                     image_to_mem_buf(img_face_replace, 'png'))
         else:
@@ -60,9 +62,23 @@ async def face_replace_handler(message, url):
     img_final = Image.open(img_file)
 
     for (x, y, w, h) in retrieve_faces(BytesIO(response.content)):
+        eyes = retrieve_eyes_on_face(
+                BytesIO(response.content), x, y, w, h)
+
+        if len(eyes) == 2:
+            # computes tilt of eyes to guestimate face tilt
+            deltaX = eyes[0][0] - eyes[1][0]
+            deltaY = eyes[1][1] - eyes[0][1] # swap index for image y coords
+            deg = math.degrees(math.atan(deltaY / deltaX))
+        elif len(eyes) == 3:
+            await client.send_message(message.channel, 'Illuminati confirmed.')
+        else:
+            deg = 180 # for giggles
+
         temp = img_face_replace
         height = h * (img_face_replace.height/img_face_replace.width)
         temp = temp.resize((w, int(height)))
+        temp = temp.rotate(deg, resample=Image.BICUBIC)
         img_final.paste(temp, (x, y), mask=temp)
 
     await client.send_file(message.channel, image_to_mem_buf(img_final, 'png'))
@@ -71,16 +87,40 @@ async def face_replace_handler(message, url):
 # Retrieves all coordinates of faces on given image
 #
 def retrieve_faces(image_fp):
+    cv_mat_gray = mem_buf_to_cv2_mat(image_fp)
+
+    # object detection using face cascade on cv matrix
+    faces = face_cascade.detectMultiScale(cv_mat_gray, 1.1, 5)
+
+    print("faces", faces)
+
+    return faces
+
+#
+# Parameter: a face (x, y, w, h)
+# Output: vector of eyes
+#
+def retrieve_eyes_on_face(image_fp, x, y, w, h):
+    cv_mat_gray = mem_buf_to_cv2_mat(image_fp)
+    cv_mat_face = cv_mat_gray[y:y+h, x:x+w]
+
+    eyes = eye_cascade.detectMultiScale(cv_mat_face)
+
+    print("eyes", eyes)
+
+    return eyes
+
+#
+# Transforms Image in buffer to cv2 matrix
+#
+def mem_buf_to_cv2_mat(image_fp):
     # image file as byte array
     image_buf = np.asarray(bytearray(image_fp.read()), dtype=np.uint8)
 
     # image buffer to cv matrix
     cv_mat_gray = cv2.imdecode(image_buf, cv2.IMREAD_GRAYSCALE)
 
-    # object detection using face cascade on cv matrix
-    faces = face_cascade.detectMultiScale(cv_mat_gray, 1.1, 5)
-
-    return faces
+    return cv_mat_gray
 
 #
 # Transforms pillow Image to in-memory buffer image
